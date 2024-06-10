73aa4ff55bac520a9703e9bf5d370c84f18d790c
isavara <161192611+isavara@users.noreply.github.com>
2024-06-10 12:11:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,59 +1,76 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/container"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+        "fmt"
+        "os"
+        "github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/container"
+        "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		engineVersions, err := container.GetEngineVersions(ctx, &container.GetEngineVersionsArgs{})
-		if err != nil {
-			return err
-		}
-		masterVersion := engineVersions.LatestMasterVersion
+    // Get environment variables
+    clusterName := os.Getenv("GKE_CLUSTER_NAME")
+    network := os.Getenv("NETWORK")
+    subnetwork := os.Getenv("SUB_NETWORK")
+    machineType := os.Getenv("MACHINE_TYPE")
+    nodeCount := os.Getenv("NODE_COUNT")
+    	
+	// Print environment variables for debugging
+    fmt.Println("GKE_CLUSTER_NAME:", clusterName)
+    fmt.Println("NETWORK:", network)
+    fmt.Println("SUB_NETWORK:", subnetwork)
+    fmt.Println("MACHINE_TYPE:", machineType)
+    fmt.Println("NODE_COUNT:", nodeCount)
+    	
+        pulumi.Run(func(ctx *pulumi.Context) error {
 
-		cluster, err := container.NewCluster(ctx, "siva-demo-cluster-1", &container.ClusterArgs{
-			DeletionProtection: pulumi.Bool(false),
-			Network:            pulumi.String("hclsw-bigfix-pub-vpc-us-east-4"),
-			Subnetwork:         pulumi.String("hclsw-bigfix-pub-subnet-us-east-4a"),
-			InitialNodeCount:   pulumi.Int(2),
-			MinMasterVersion:   pulumi.String(masterVersion),
-			NodeVersion:        pulumi.String(masterVersion),
-			NodeConfig: &container.ClusterNodeConfigArgs{
-				MachineType: pulumi.String("e2-standard-16"),
-				OauthScopes: pulumi.StringArray{
-					pulumi.String("https://www.googleapis.com/auth/compute"),
-					pulumi.String("https://www.googleapis.com/auth/devstorage.read_only"),
-					pulumi.String("https://www.googleapis.com/auth/logging.write"),
-					pulumi.String("https://www.googleapis.com/auth/monitoring"),
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
+                engineVersions, err := container.GetEngineVersions(ctx, &container.GetEngineVersionsArgs{})
+                if err != nil {
+                        return err
+                }
+                masterVersion := engineVersions.LatestMasterVersion
 
-		ctx.Export("kubeconfig", generateKubeconfig(cluster.Endpoint, cluster.Name, cluster.MasterAuth))
+                cluster, err := container.NewCluster(ctx, clusterName, &container.ClusterArgs{
+                        DeletionProtection: pulumi.Bool(false),
+                        Network:            pulumi.String(network),
+                        Subnetwork:         pulumi.String(subnetwork),
+                        InitialNodeCount:   pulumi.Int(nodeCount),
+                        MinMasterVersion:   pulumi.String(masterVersion),
+                        NodeVersion:        pulumi.String(masterVersion),
+                        NodeConfig: &container.ClusterNodeConfigArgs{
+                                MachineType: pulumi.String(machineType),
+                                OauthScopes: pulumi.StringArray{
+                                        pulumi.String("https://www.googleapis.com/auth/compute"),
+                                        pulumi.String("https://www.googleapis.com/auth/devstorage.read_only"),
+                                        pulumi.String("https://www.googleapis.com/auth/logging.write"),
+                                        pulumi.String("https://www.googleapis.com/auth/monitoring"),
+                                },
+                        },
+                })
+                if err != nil {
+                        return err
+                }
 
-		// Uncomment and use the provider if needed
-		// k8sProvider, err := kubernetes.NewProvider(ctx, "k8sprovider", &kubernetes.ProviderArgs{
-		// 	Kubeconfig: generateKubeconfig(cluster.Endpoint, cluster.Name, cluster.MasterAuth),
-		// }, pulumi.DependsOn([]pulumi.Resource{cluster}))
-		// if err != nil {
-		// 	return err
-		// }
+                ctx.Export("kubeconfig", generateKubeconfig(cluster.Endpoint, cluster.Name, cluster.MasterAuth))
 
-		return nil
-	})
+                // Uncomment and use the provider if needed
+                // k8sProvider, err := kubernetes.NewProvider(ctx, "k8sprovider", &kubernetes.ProviderArgs{
+                //      Kubeconfig: generateKubeconfig(cluster.Endpoint, cluster.Name, cluster.MasterAuth),
+                // }, pulumi.DependsOn([]pulumi.Resource{cluster}))
+                // if err != nil {
+                //      return err
+                // }
+
+                return nil
+        })
 }
 
 func generateKubeconfig(clusterEndpoint pulumi.StringOutput, clusterName pulumi.StringOutput,
-	clusterMasterAuth container.ClusterMasterAuthOutput) pulumi.StringOutput {
-	context := pulumi.Sprintf("demo_%s", clusterName)
+        clusterMasterAuth container.ClusterMasterAuthOutput) pulumi.StringOutput {
+        context := pulumi.Sprintf("demo_%s", clusterName)
 
-	return pulumi.Sprintf(`apiVersion: v1
+        return pulumi.Sprintf(`apiVersion: v1
 clusters:
 - cluster:
     certificate-authority-data: %s
@@ -77,7 +94,6 @@ users:
         https://cloud.google.com/blog/products/containers-kubernetes/kubectl-auth-changes-in-gke
       provideClusterInfo: true
 `,
-		clusterMasterAuth.ClusterCaCertificate().Elem(),
-		clusterEndpoint, context, context, context, context, context, context)
+                clusterMasterAuth.ClusterCaCertificate().Elem(),
+                clusterEndpoint, context, context, context, context, context, context)
 }
-
